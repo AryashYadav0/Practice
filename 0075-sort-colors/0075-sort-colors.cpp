83e4284d33da0ec27f135d54c0502a9f22bb346c
Abhishek Yadav <155575265+AryashYadav0@users.noreply.github.com>
2025-07-14 17:31:25 +0530

    Time: 0 ms (100%), Space: 11.6 MB (78.96%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -17,14 +17,24 @@ public:
         // }
 
         // selection sort 
-        for(int i= 0; i<n-1; i++ ){
-            int smallInd = i;
-            for(int j = i+1; j<n; j++){
-                if(nums[smallInd]>nums[j]){
-                    smallInd= j;
-                }
+        // for(int i= 0; i<n-1; i++ ){
+        //     int smallInd = i;
+        //     for(int j = i+1; j<n; j++){
+        //         if(nums[smallInd]>nums[j]){
+        //             smallInd= j;
+        //         }
+        //     }
+        //     swap(nums[i], nums[smallInd]);
+        // }
+
+        // insertion sort
+        for(int i =1; i<n; i++){
+            int curr = nums[i], prev= i-1;
+            while(prev>=0 && nums[prev]>curr){
+                nums[prev+1] = nums[prev];
+                prev--;
             }
-            swap(nums[i], nums[smallInd]);
+            nums[prev+1]= curr;
         }
     }
 };
