8385ecdd0295d094f80b4c5809499efb5199523a
Abhishek Yadav <155575265+AryashYadav0@users.noreply.github.com>
2025-07-14 17:17:34 +0530

    Time: 2 ms (3.49%), Space: 11.7 MB (12.47%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -2,17 +2,29 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
         int n = nums.size();
-        bool isSwap = false ;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (nums[j] > nums[j + 1]) {
-                    swap(nums[j], nums[j + 1]);
-                    isSwap= true;
+        // bubble sort 
+        // bool isSwap = false ;
+        // for (int i = 0; i < n - 1; i++) {
+        //     for (int j = 0; j < n - i - 1; j++) {
+        //         if (nums[j] > nums[j + 1]) {
+        //             swap(nums[j], nums[j + 1]);
+        //             isSwap= true;
+        //         }
+        //     }
+        //     if(!isSwap){
+        //         return ;
+        //     }
+        // }
+
+        // selection sort 
+        for(int i= 0; i<n-1; i++ ){
+            int smallInd = i;
+            for(int j = i+1; j<n; j++){
+                if(nums[smallInd]>nums[j]){
+                    smallInd= j;
                 }
             }
-            if(!isSwap){
-                return ;
-            }
+            swap(nums[i], nums[smallInd]);
         }
     }
 };
