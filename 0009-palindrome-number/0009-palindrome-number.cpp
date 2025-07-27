0f92d1f6664b23ca23dafee723995dc6383116fb
Abhishek Yadav <155575265+AryashYadav0@users.noreply.github.com>
2025-07-27 08:50:19 +0530

    Time: 3 ms (45.9%), Space: 8.4 MB (99.62%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -3,7 +3,20 @@ public:
     bool isPalindrome(int x) {
         int r = 0;
         if (x < 0 || (x % 10 == 0 && x)) return false;
-        while (x > r) r = r * 10 + x % 10, x /= 10;
+        while (x > r){ 
+            r = r * 10 + x % 10;
+            x /= 10;
+        }
         return x == r || x == r / 10;
+
+        // if(x<0 || (x%10 ==0 && x)) return false; 
+        // int rev = 0, org = x;
+        // while(x!=0){
+        //     int digit = x%10;
+        //     rev = rev *10 +digit;
+        //     x/=10;
+        // }
+        // if(rev==org) return true;
+        // else  return false ;
     }
 };
