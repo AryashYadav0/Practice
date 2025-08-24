a2acf277699010c31cf161ff914cf6ca077090ec
Abhishek Yadav <155575265+AryashYadav0@users.noreply.github.com>
2025-08-24 18:07:28 +0530

    Time: 2 ms (77.22%), Space: 7.8 MB (63.96%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,8 +1,17 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n<=1) return n;
-        return fib(n-1) + fib(n-2);
+        // if(n<=1) return n;
+        // return fib(n-1) + fib(n-2);
+        if(n==0) return n;
+        int f = 0, s= 1, tmp;
+        for (int i = 2; i<=n; i++){
+            tmp =f+s;
+            f = s;
+            s = tmp; 
+        }
+        return s;
+
         
     }
 };
